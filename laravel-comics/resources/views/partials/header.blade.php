167646c5128d22ebec1e717ac909d48a50a55a6a
steftes99 <stefanotescione83@gmail.com>
2021-10-25 17:24:51 +0200

    fine header

--- laravel-comics/resources/views/partials/header.blade.php
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -4,22 +4,25 @@
     </div>
     <div class="container">
         <img src="{{asset('img/dc-logo.png')}}" alt="logo-dc">
+    
+
+        <nav>
+            <ul>
+                <li class="active"><a href="{{route('home')}}"> Characters </a></li>
+                <li><a href="{{route('comics')}}"> Comics </a></li>
+                <li><a href="{{route('movies')}}"> Movies </a></li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="jumbo">
+        
     </div>
 
-    <nav>
-        <ul>
-            <li class="active"><a href="{{route('home')}}"> Characters </a></li>
-            <li><a href="{{route('comics')}}"> Comics </a></li>
-            <li><a href="{{route('movies')}}"> Movies </a></li>
-            <li>Tv</li>
-            <li>Games</li>
-            <li>Collectibles</li>
-            <li>Videos</li>
-            <li>Fans</li>
-            <li>News</li>
-            <li>Shop</li>
-        </ul>
-    </nav>
-
-    
 </header>
