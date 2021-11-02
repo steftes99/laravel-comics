ed02e5319b4d89811aeaf009fdea5f750f2565b7
steftes99 <stefanotescione83@gmail.com>
2021-11-02 10:53:48 +0100

    fine sezione comic singolo

--- laravel-comics/resources/views/comic.blade.php
+++ laravel-comics/resources/views/comic.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{$comic['thumb']}}" :alt="{{$comic['series']}}">
             </div>
             <div class="center">
-                <h1>{{$comic['title']}}</h1>
+                <h2>{{$comic['title']}}</h2>
                 <div class="price-container">
                     <p>U.S. Price : </p> <span>{{$comic['price']}}</span>
                     <span class="avalaible">Avalaible</span>
@@ -23,14 +23,49 @@
         <div class="bottom">
             <div class="bottom-left">
                 <h2>Talent</h2>
-                <p>Art by: </p>
-                <p>Written by</p>
+                <hr>
+                <div class="info-container">
+                    <p>Art by: </p>
+                    <p class="artist">
+                        @foreach ($comic["artists"] as $artist)
+                            {{$artist . ','}} 
+                        @endforeach
+                    </p>
+                    
+                </div>
+
+                <hr>
+               
+                <div class="info-container">
+                    <p>Written by:</p>
+                    <p class="artist">
+                        @foreach ($comic['writers'] as $writer)
+                            {{$writer . ','}}
+                        @endforeach
+                    </p>
+                    
+                </div>
+                <hr>
+              
             </div>
             <div class="bottom-right">
                 <h2>Specs</h2>
-                <p>Series: {{$comic['series']}}</p>
-                <p>U.S. Price: {{$comic['price']}}</p>
-                <p>On Sale Date: {{$comic['sale_date']}}</p>
+                <hr>
+                <div class="info">
+                    <p>Series:</p> <p class="series">{{$comic['series']}}</p>
+                </div>
+                
+                <hr>
+                <div class="info">
+                    <p>U.S. Price:</p> <p>{{$comic['price']}}</p> 
+                </div>
+                
+                <hr>
+                <div class="info">
+                    <p>On Sale Date:</p> <p>{{$comic['sale_date']}}</p> 
+                </div>
+                
+                <hr>
             </div>
         </div>
         
